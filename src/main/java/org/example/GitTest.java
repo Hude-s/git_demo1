dd95080f13db929e0d8ea28cc1e57eaefd3fe537
Hudes <74709540+Hude-s@users.noreply.github.com>
2022-08-08 14:47:38 +0800

    Update GitTest.java

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("ssh push test");
+        System.out.println("ssh pull test");
+        
     }
 }
