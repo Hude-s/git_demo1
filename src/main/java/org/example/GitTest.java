5d66f1a33b57f8c28f2751c01a2861312bfc7c1f
yh <2784559867@qq.com>
2022-08-08 14:00:54 +0800

    first idea git commit

---
+++ src/main/java/org/example/GitTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git测试");
+        System.out.println("git测试");
+        System.out.println("git测试");
+    }
+}
