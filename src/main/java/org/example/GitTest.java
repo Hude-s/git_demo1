1f59ba8ac06071e61f750093c97513a3550ba4fd
yh <2784559867@qq.com>
2022-08-08 14:03:57 +0800

    hot-fix  modify submit idea git commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("git测试");
         System.out.println("git测试");
         System.out.println("git测试");
+        System.out.println("hot-fix提交");
     }
 }
